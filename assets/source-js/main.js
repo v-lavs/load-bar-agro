771b57f7ca55f288c8d755c86e1d4ecff16a7dcc
v-lavs <lavskavanda@gmail.com>
2018-08-17 12:26:50 +0300

    add file

---
+++ assets/source-js/main.js
@@ -0,0 +1,56 @@
+jQuery(document).ready(function($) {
+  // jQuery("#animated")
+  // function move() {
+  //   var elem = document.getElementById("animated");
+  //   var width = 1;
+  //   var id = setInterval(frame, 10);
+  //   function frame() {
+  //     if (width >= 100) {
+  //       clearInterval(id);
+  //     } else {
+  //       width++;
+  //       elem.style.width = width + '%';
+  //     }
+  //   }
+  // }
+
+  // var duration = 600; //'slow'
+  // $(".test").each(function(index) {
+  //   $(this).delay(duration * index).show(duration);
+  // });
+
+  // jQuery('.one')
+  //   // .delay(800)
+  //   .show('200')
+  // jQuery('.two')
+  // .delay(800)
+  //   .stretchLeft('200')
+
+  // ;(function($){
+  //   $.fn.fadeInDelay = function(){
+  //     var init = function(){
+  //       $(this).hide().delay($(this).data('delay')).fadeIn();
+  //     };
+  //     return this.each(init);
+  //   };
+  // }(jQuery));
+  //
+  // $('div').fadeInDelay();
+
+  // jQuery( "#animation_block1" ).animate({
+  //
+  //   opacity: 0.5,
+  //   left: "+=50",
+  //   height: "toggle"
+  // },  function() {
+  //   // Animation complete.
+  // });
+  //
+  // jQuery( "#animation_block2" ).animate({
+  //   opacity: 0.5,
+  //   right: "+=50",
+  //   height: "toggle"
+  // },  function() {
+  //   // Animation complete.
+  // });
+});
