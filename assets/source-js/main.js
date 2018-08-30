a94b36519005ef02a5c0ff20fdd33daf97d6e223
v-lavs <lavskavanda@gmail.com>
2018-08-30 08:33:09 +0300

    add new preloader2

--- assets/source-js/main.js
+++ assets/source-js/main.js
@@ -1,56 +1,29 @@
-jQuery(document).ready(function($) {
-  // jQuery("#animated")
-  // function move() {
-  //   var elem = document.getElementById("animated");
-  //   var width = 1;
-  //   var id = setInterval(frame, 10);
-  //   function frame() {
-  //     if (width >= 100) {
-  //       clearInterval(id);
-  //     } else {
-  //       width++;
-  //       elem.style.width = width + '%';
-  //     }
-  //   }
-  // }
-
-  // var duration = 600; //'slow'
-  // $(".test").each(function(index) {
-  //   $(this).delay(duration * index).show(duration);
-  // });
-
-  // jQuery('.one')
-  //   // .delay(800)
-  //   .show('200')
-  // jQuery('.two')
-  // .delay(800)
-  //   .stretchLeft('200')
-
-  // ;(function($){
-  //   $.fn.fadeInDelay = function(){
-  //     var init = function(){
-  //       $(this).hide().delay($(this).data('delay')).fadeIn();
-  //     };
-  //     return this.each(init);
-  //   };
-  // }(jQuery));
-  //
-  // $('div').fadeInDelay();
-
-  // jQuery( "#animation_block1" ).animate({
-  //
-  //   opacity: 0.5,
-  //   left: "+=50",
-  //   height: "toggle"
-  // },  function() {
-  //   // Animation complete.
-  // });
-  //
-  // jQuery( "#animation_block2" ).animate({
-  //   opacity: 0.5,
-  //   right: "+=50",
-  //   height: "toggle"
-  // },  function() {
-  //   // Animation complete.
-  // });
-});
+jQuery(document).ready(function ($) {
+  /**
+   * PRELOADER
+//    **/
+//   document.body.on('load', function(){
+//     setTimeout(function () {
+//       var preloader = document.getElementById('page-preloader');
+//       if (!preloader.classList.contains('done')) {
+//         preloader.classList.add('done');
+//       }
+//     }, 1000);
+//   });
+//   /**
+//    * POPUP
+//    **/
+//   jQuery('#popup-trigger').click(function (event) {
+//     event.preventDefault();
+//
+//     jQuery('#popup').addClass('active');
+//     jQuery('#overlay').fadeIn();
+//
+//     jQuery('#popup-close, #overlay').click(function (event) {
+//       event.preventDefault();
+//       jQuery('#popup').removeClass('active');
+//       jQuery('#overlay').fadeOut();
+//     });
+//   });
+//
+// });
