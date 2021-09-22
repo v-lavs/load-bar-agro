9157137f897b79fd07984505cece91bcee21fd28
v-lavs <lavskavanda@gmail.com>
2021-09-22 19:36:53 +0300

    add new preloader

--- assets/source-js/main.js
+++ assets/source-js/main.js
@@ -1,29 +1,49 @@
 jQuery(document).ready(function ($) {
   /**
    * PRELOADER
-//    **/
-//   document.body.on('load', function(){
-//     setTimeout(function () {
-//       var preloader = document.getElementById('page-preloader');
-//       if (!preloader.classList.contains('done')) {
-//         preloader.classList.add('done');
-//       }
-//     }, 1000);
-//   });
-//   /**
-//    * POPUP
-//    **/
-//   jQuery('#popup-trigger').click(function (event) {
-//     event.preventDefault();
-//
-//     jQuery('#popup').addClass('active');
-//     jQuery('#overlay').fadeIn();
-//
-//     jQuery('#popup-close, #overlay').click(function (event) {
-//       event.preventDefault();
-//       jQuery('#popup').removeClass('active');
-//       jQuery('#overlay').fadeOut();
-//     });
-//   });
-//
-// });
+  **/
+
+  // $(window).on('load', function () {
+  //   $('#page-preloader').delay(2000).queue(function() { $(this).remove(); });
+  // })
+
+ //  /**
+ //   * POPUP
+ //   **/
+ //  jQuery('#popup-trigger').click(function (event) {
+ //    event.preventDefault();
+ //
+ //    jQuery('#popup').addClass('active');
+ //    jQuery('#overlay').fadeIn();
+ //
+ //    jQuery('#popup-close, #overlay').click(function (event) {
+ //      event.preventDefault();
+ //      jQuery('#popup').removeClass('active');
+ //      jQuery('#overlay').fadeOut();
+ //    });
+ //  });
+ //
+ // /*
+ // * SLIDE
+ // */
+ //  /**
+ //   * ANIMATION ABOUT
+ //   **/
+ //
+ //  $(document).ready(function () {
+ //    var h = $(window).height();
+ //    $(".about .print-top").addClass('startSlideDown');
+ //    setTimeout(function () {
+ //      $(".about .print-top").addClass('endSlideDown');
+ //    }, 4000);
+ //
+ //    $(window).scroll(function () {
+ //      if (($(this).scrollTop()) + h >= $(".about").offset().top) {
+ //      }
+ //    });
+ //  });
+});
+
+
+
+
